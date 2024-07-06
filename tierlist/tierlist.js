73d145aa73d2ac53b32170f5c43cf20c8393561d
Zack Broderson <zbroderson@hotmail.com>
2024-07-06 17:00:43 -0400

    Tierlist drag and drop functionality

--- tierlist/tierlist.js
+++ tierlist/tierlist.js
@@ -1,32 +1,72 @@
 import mal from "../mal.js";
-const container = document.getElementById("test");
 const backend = await browser.runtime.getBackgroundPage();
-
-renderRanks();
 
 const animeList = await mal.getAnimeList();
 console.log(animeList);
 
+const unrankedAnime = {};
+const rankedAnime = {};
+mapAnimeObjects();
+
+
+renderRanks();
+
+
 renderUnrankedAnime();
 
-// instead of stacking the tiers on top of the unranked anime we should just reduce the length of the tiers and put the unranked on the side
-// combining tiers 1-3 and maybe 4-5 into a single line may be helpful for space
+function dragOverHandler(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+}
+
+function mapAnimeObjects() {
+    for (const anime of animeList) {
+        const mappedAnime = {
+            id: anime.node.id,
+            title: anime.node.alternative_titles.en ? anime.node.alternative_titles.en : anime.node.title,
+            coverUrl: anime.node.main_picture.medium,
+            score: anime.list_status.score
+        }
+
+        if (anime.score > 0)
+            rankedAnime[mappedAnime.id] = mappedAnime;
+        else
+            unrankedAnime[mappedAnime.id] = mappedAnime;
+    }
+
+}
+
 function renderRanks() {
     const container = document.getElementById("tiers-container");
-//    for (let index = 1; index < 11; index++) {
-//        const tierElement = document.createElement("div");
-//        tierElement.classList.add("tier");
-//
-//        const numberElement = document.createElement("div");
-//        numberElement.textContent = index;
-//        tierElement.appendChild(numberElement);
-//
-//        container.appendChild(tierElement);
-//    }
+
+
+    const dropHandler = (tier) => (e) => {
+        e.preventDefault();
+
+        const id = e.dataTransfer.getData("text/plain");
+
+        const tierContainer = document.getElementById(tier);
+        const animeContainer = document.getElementById(id);
+
+        if (animeContainer.parentElement !== tierContainer)
+        {
+            tierContainer.appendChild(animeContainer);
+
+            if (unrankedAnime[id]) {
+                const anime = unrankedAnime[id];
+                unrankedAnime[id] = undefined;
+                rankedAnime[id] = anime;
+            }        
+        }
+
+    }
 
     for (let i = 10; i  > 4; i--) {
         const tierElement = document.createElement("div");
+        tierElement.setAttribute("id", i);
         tierElement.classList.add("tier");
+        tierElement.addEventListener("dragover", dragOverHandler);
+        tierElement.addEventListener("drop", dropHandler(i));
 
         const numberElement = document.createElement("div");
         numberElement.textContent = i;
@@ -35,44 +75,81 @@ function renderRanks() {
         container.appendChild(tierElement);
     }
 
-    for (let i = 4; i  > 0; i-=2) {
-        const multiTierContainer = document.createElement("div");
-        multiTierContainer.classList.add("multi-tier");
-
-        const firstTierElement = document.createElement("div");
-        firstTierElement.classList.add("tier");
-
-        let numberElement = document.createElement("div");
-        numberElement.textContent = i;
-        firstTierElement.appendChild(numberElement);
-
-        const secondTierElement = document.createElement("div");
-        secondTierElement.classList.add("tier");
-
-        numberElement = document.createElement("div");
-        numberElement.textContent = i - 1;
-        secondTierElement.appendChild(numberElement);
+    let multiTierContainer;
+    for (let i = 4; i  > 0; i--) {
+        if (!multiTierContainer) {
+            multiTierContainer = document.createElement("div");
+            multiTierContainer.classList.add("multi-tier");
+        }
         
-        multiTierContainer.append(firstTierElement, secondTierElement);
+        const tierElement = document.createElement("div");
+        tierElement.setAttribute("id", i);
+        tierElement.classList.add("tier");
+        tierElement.addEventListener("dragover", dragOverHandler);
+        tierElement.addEventListener("drop", dropHandler(i));
 
-        container.appendChild(multiTierContainer);
+        const numberElement = document.createElement("div");
+        numberElement.textContent = i;
+        tierElement.appendChild(numberElement);
+        
+        multiTierContainer.appendChild(tierElement);
+
+        if ((i+1) % 2 === 0 && multiTierContainer)
+        {
+            container.appendChild(multiTierContainer);
+            multiTierContainer = null;
+        }
     }
-
 }
 
 function renderUnrankedAnime() {
     const container = document.getElementById("unranked-container");
+    container.textContent = "";
+
+    const dropHandler = (e) => {
+        e.preventDefault();
+
+        const id = e.dataTransfer.getData("text/plain");
+
+        const animeContainer = document.getElementById(id);
+
+        if (animeContainer.parentElement !== container)
+        {
+            if (rankedAnime[id]) {
+                const anime = rankedAnime[id];
+                rankedAnime[id] = undefined;
+                unrankedAnime[id] = anime;
+            }        
+
+            animeContainer.remove();
+            renderUnrankedAnime();
+        }
+
+    }
+
+    container.removeEventListener("dragover", dragOverHandler);
+    container.addEventListener("dragover", dragOverHandler);
+    container.removeEventListener("drop", dropHandler);
+    container.addEventListener("drop", dropHandler);
+
+    const dragStartHandler = (id) => (e) => {
+        e.dataTransfer.setData("text/plain", id);
+    }
 
     let index = 0;
-    for (const anime of animeList) {
+    const sortedAnime = Object.values(unrankedAnime).sort((a,b) => a.title.localeCompare(b.title));
+    
+    for (const anime of sortedAnime) {
         index++;
         const animeContainer = document.createElement("div");
+        animeContainer.setAttribute("id", anime.id);
+        animeContainer.setAttribute("draggable", true);
         animeContainer.classList.add("anime");
+        animeContainer.addEventListener("dragstart", dragStartHandler(anime.id));
 
-        const name = anime.node.alternative_titles.en ? anime.node.alternative_titles.en : anime.node.title;
         const imgElement = document.createElement("img");
-        imgElement.setAttribute("src", anime.node.main_picture.medium);
-        imgElement.setAttribute("title", name);
+        imgElement.setAttribute("src", anime.coverUrl);
+        imgElement.setAttribute("title", anime.title);
 
         animeContainer.appendChild(imgElement);
         container.appendChild(animeContainer);
